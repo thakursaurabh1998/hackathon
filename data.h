58e865eed54343fab29039d9e89c9d029fc66b46
shrutisheoran <shruti.sheoran03@gmail.com>
2017-10-28 11:27:34 +0530

    data

--- data.h
+++ data.h
@@ -1,10 +1,24 @@
 #include<iostream>
-#include<cstdio>
-#include<>
+#include<stdio>
+
+using namespace std;
 
 void hospital()
 {
-	
-	char* hos_name = (char*) malloc(n*sizeof(char));
+	char hos_name[11][25];
+	int hos_sector[11];
+	char hos_price[11]="Not Known";
+	float hos_rating[11];
+	char hos_avail[5]="Open";
+	hos_name[0]="Alchemist";
 	
 }
+void hotel()
+{
+	char hotel_name[11][25];
+	int hotel_sector[11];
+	int hotel_price[11];
+	float hotel_rating[11];
+	char hotel_avail[11][6];
+}
+
